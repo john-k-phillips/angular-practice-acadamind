a4348880f6235cb7e6de38b3610a10f1cc1f20d8
John Phillips <johnphillips.dev@gmail.com>
2021-11-29 11:53:02 +0000

    Recipe ingredients now display

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,19 +1,30 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
-      'Test Recipe',
-      'This is a test recipe.',
-      'https://production-media.gousto.co.uk/cms/mood-image/1308---Crispy-Baharat-Chicken--Spiced-Potatoes-x700.jpg'
+      'Baharat Chicken With Batata Harra And Sumac Yoghurt',
+      "Combining crispy baharat spiced chicken thighs with 'batata harra' – a tasty Middle Eastern potato dish seasoned with lemon, fresh herbs and rich spices – this dish is packed with flavour. Served with green beans and a dollop of zingy sumac yoghurt to the side.",
+      'https://production-media.gousto.co.uk/cms/mood-image/1308---Crispy-Baharat-Chicken--Spiced-Potatoes-x700.jpg',
+      [
+        new Ingredient('Tomato', 2),
+        new Ingredient('Fries', 2),
+        new Ingredient('Meat', 2),
+      ]
     ),
 
     new Recipe(
-      'Test Recipe',
-      'This is a test recipe.',
-      'https://production-media.gousto.co.uk/cms/mood-image/1308---Crispy-Baharat-Chicken--Spiced-Potatoes-x700.jpg'
+      'Chicken, Date & Tamarind Curry With Kachumber',
+      "to create a luxurious chicken curry, you'll infuse rich coconut stock with sweet dates, ginger and tangy tamarind. Serve over buttery basmati rice with a cucumber, tomato and coriander salad.",
+      'https://production-media.gousto.co.uk/cms/mood-image/687---Chicken-Date--Tamarind-Curry_01-1592819703164-x700.jpg',
+      [
+        new Ingredient('Chicken', 12),
+        new Ingredient('Tomato', 5),
+        new Ingredient('Lettuce', 23),
+      ]
     ),
   ];
   getRecipes() {
