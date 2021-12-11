9dd86f5110365d4e3cf318f7583196f21392f6d0
John Phillips <johnphillips.dev@gmail.com>
2021-12-11 16:15:23 +0000

    Recipe project correctly updates details and deletes.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -45,4 +45,9 @@ export class RecipeService {
     this.recipes[index] = recipe;
     this.recipesChange.next(this.recipes.slice());
   }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChange.next(this.recipes.slice());
+  }
 }
