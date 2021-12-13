df624ad74a39582b65fc87082da56d6d08f6fc8c
John Phillips <johnphillips.dev@gmail.com>
2021-12-13 00:22:27 +0000

    Bug fixes with fetching data via routing.

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 
 import { Recipe } from '../recipes/recipe.model';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -19,8 +20,18 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get<Recipe[]>(`${this.url}recipes.json`).subscribe((recipes) => {
-      this.recipeService.setRecipes(recipes);
-    });
+    return this.http.get<Recipe[]>(`${this.url}recipes.json`).pipe(
+      map((recipes) => {
+        return recipes.map((recipe) => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : [],
+          };
+        });
+      }),
+      tap((recipes) => {
+        this.recipeService.setRecipes(recipes);
+      })
+    );
   }
 }
